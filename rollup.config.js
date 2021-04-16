061fa97dc6c2d450348d42f3772fd53c03ac5549
Jack <git@5079.ml>
2021-04-15 23:09:30 -0400

    fix

--- rollup.config.js
+++ rollup.config.js
@@ -47,8 +47,7 @@ export default {
       preprocess: sveltePreprocess({ sourceMap: !production }),
       compilerOptions: {
         // enable run-time checks when not in production
-        dev: !production,
-        cssHash: ({ filename }) => 'moller-' + filename
+        dev: !production
       }
     }),
     // we'll extract any component CSS out into
