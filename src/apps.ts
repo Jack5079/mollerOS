33c38e9081ef8072bbe28b80cfc4b395497f566b
Jack <git@5079.ml>
2021-04-21 17:14:59 -0400

    oh my fucking god

--- src/apps.ts
+++ src/apps.ts
@@ -1,4 +1,5 @@
 import Settings from './apps/Settings.svelte'
+import type { App } from './types'
 import Terminal from './apps/Terminal.svelte'
 import Dog from './apps/dog.svelte'
 import Spuchbop from './apps/spuchbop.svelte'
@@ -9,22 +10,20 @@ import FileExplorer from './apps/FileExplorer/FileExplorer.svelte'
 import Trollface from './apps/Trollface.svelte'
 import Reset from './apps/Reset.svelte'
 
-import type { App } from './types'
-import type { SvelteComponentDev } from 'svelte/internal'
-
 const emoji = (char: string) => `data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>${char}</text></svg>`
 
-const app = (component: SvelteComponentDev, icon: string = 'icon.svg'): App => ({
-  name: component.name,
-  component,
-  icon: icon.length < 6 ? emoji(icon) : icon // Shortest possible link is "//a.ml" so anything shorter is an emoji
-})
-
 const apps: App[] = [
-  app(Terminal, 'https://upload.wikimedia.org/wikipedia/commons/0/01/Windows_Terminal_Logo_256x256.png'),
-  app(DaBaby, 'https://upload.wikimedia.org/wikipedia/en/9/98/Dababy_BabyOnBaby.jpg'),
-  app(Hydra, 'https://windows93.net/c/sys/skins/w93/apps/hydra.png'),
-  app(Settings),
+  {
+    name: 'Settings',
+    component: Settings,
+    icon: 'icon.svg'
+  },
+  {
+    name: 'Terminal',
+    component: Terminal,
+    icon:
+      'https://upload.wikimedia.org/wikipedia/commons/0/01/Windows_Terminal_Logo_256x256.png'
+  },
   {
     name: 'dog',
     component: Dog,
@@ -41,6 +40,16 @@ const apps: App[] = [
     component: SessionManager,
     icon:
       'https://winaero.com/blog/wp-content/uploads/2019/03/task-manager-taskmgr-icon-big.png'
+  },
+  {
+    name: 'DaBaby',
+    component: DaBaby,
+    icon: 'https://upload.wikimedia.org/wikipedia/en/9/98/Dababy_BabyOnBaby.jpg'
+  },
+  {
+    name: 'Hydra',
+    component: Hydra,
+    icon: 'https://windows93.net/c/sys/skins/w93/apps/hydra.png'
   },
   {
     name: 'File Explorer',
