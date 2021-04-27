75d63ab0b56c62761013a68304769ac81e9c5f46
Jack <git@5079.ml>
2021-04-27 14:17:22 -0400

    add monaco finally
    note: VERY slow build

--- src/apps/FileExplorer/viewers/Editor.svelte
+++ src/apps/FileExplorer/viewers/Editor.svelte
@@ -1,29 +1,51 @@
 <script lang="ts">
   export let file: string
   import fs from '../../../fs'
-  let value: string
-  fs.promises
-    .readFile(file, {
+  import { editor, languages } from 'monaco-editor'
+  import { onMount } from 'svelte'
+  import path from '@jkearl/lightning-fs/src/path'
+  let div: HTMLDivElement
+
+  export const languageOf = (filename: string) =>
+    languages
+      .getLanguages()
+      .find(
+        (lanugage) =>
+          lanugage.filenames?.some((fname) => fname === filename) ||
+          lanugage.extensions?.some((ext) => filename.endsWith(ext))
+      )?.id || 'plaintext'
+
+  onMount(async () => {
+    const value = await fs.promises.readFile(file, {
       encoding: 'utf8'
     })
-    .then((text) => {
-      value = (text as unknown) as string
+    const monaco = editor.create(div, {
+      automaticLayout: true,
+      value: (value as unknown) as string,
+      theme: matchMedia('(prefers-color-scheme: light)').matches
+        ? 'vs'
+        : 'vs-dark'
     })
-  $: fs.promises.writeFile(file, value)
+    monaco.onDidChangeModelContent(() => {
+      fs.promises.writeFile(file, monaco.getValue())
+    })
+    editor.setModelLanguage(monaco.getModel(), languageOf(path.basename(file)))
+    // make it actually fucking save when you change it
+    return monaco.dispose.bind(monaco)
+  })
 </script>
 
-<textarea bind:value />
+<div bind:this={div} />
 
 <style>
-  textarea {
+  div {
     width: 100%;
-    resize: none;
     border: 0;
     background: black;
     color: white;
   }
   @media (prefers-color-scheme: light) {
-    textarea {
+    div {
       background: white;
       color: black;
     }
